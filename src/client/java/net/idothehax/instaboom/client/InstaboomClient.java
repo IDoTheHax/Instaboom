0c765137e50c365729430e99db6e76aa97f78d60
IDoTheHax <hax@idothehax.com>
2025-07-16 23:00:58 +0200

    Init Commit

---
+++ src/client/java/net/idothehax/instaboom/client/InstaboomClient.java
@@ -0,0 +1,10 @@
+package net.idothehax.instaboom.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InstaboomClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/idothehax/instaboom/Instaboom.java
@@ -0,0 +1,10 @@
+package net.idothehax.instaboom;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Instaboom implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
